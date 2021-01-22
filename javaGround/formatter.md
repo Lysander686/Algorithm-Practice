66b3be3102745014a119b9ea8a298ebb037d1407
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-01-22 22:14:07 +0800

    integrate with java prettier

---
+++ javaGround/formatter.md
@@ -0,0 +1,1 @@
+prettier java [link](https://github.com/jhipster/prettier-java/blob/main/docs/advanced_usage.md)
--- javaGround/src/main/java/Main.java
+++ javaGround/src/main/java/Main.java
@@ -1,30 +1,32 @@
-
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str;
-        while ((str = bf.readLine()) != null) {
 
-            boolean[] stu = new boolean[1001];
+        while ((str = br.readLine()) != null) {
+            boolean[] flags = new boolean[1001];
             StringBuilder sb = new StringBuilder();
+
+//          输入个数
             int n = Integer.parseInt(str);
-            for (int i = 0; i < n; i++)
-                stu[Integer.parseInt(bf.readLine())] = true;
-            for (int i = 0; i < 1001; i++) {
-                if (stu[i])
-                    sb.append(i).append("\n");
+
+            for (int i = 0; i < n; i++) {
+        flags[Integer.parseInt(br.readLine())] = true;
             }
 
+            for (int i = 1; i < 1001; i++) {
+                if (flags[i]) {
+                    sb.append(i).append("\n");
+                }
+            }
 
             sb.deleteCharAt(sb.length() - 1);
             System.out.println(sb.toString());
         }
     }
 }
+
+
